3b7df6cc67e457160bbca2feedaebc689aed0664
Administrator <Administrator@pc1HMSJR2.csez.zohocorpin.com.>
2022-10-07 21:25:24 +0530

    07/10/22

---
+++ DemoRajesh/src/com/vignesh/AddServlets.java
@@ -0,0 +1,23 @@
+package com.vignesh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlets extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i +j;
+		PrintWriter out = res.getWriter();
+		out.println((k+ " is the result"));
+
+	}
+
+}
