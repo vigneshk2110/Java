180cc38d1ea4fd38feb99544b550f25aa178da8e
VigneshK <104199131+vigneshk2110@users.noreply.github.com>
2022-10-14 14:52:55 +0530

    Update succeedingInteger.java

--- Zoho/appClientModule/evaluation14Oct/succeedingInteger.java
+++ Zoho/appClientModule/evaluation14Oct/succeedingInteger.java
@@ -15,35 +15,44 @@ public class succeedingInteger {
 			size = scan.nextInt();
 		}
 		int [] array = new int[size] ;
-		
+
 		System.out.println("please enter value for each index of the array");
 
 		for (int i = 0; i < array.length; i++) {
 
 			array[i] = scan.nextInt();
 		}
-		
-		succeedInt(array);
+
+		sucInt(array);
 	}
 
-	private static void succeedInt(int[] array) {
-		String num = "";
-		
+	private static void sucInt(int[] array) {
+		int num = 0;
 		for (int i = 0; i < array.length; i++) {
+			num = num*10;
 			num = num+array[i];
 		}
-		
-		String sucInt = ""+ (Integer.parseInt(num) +1) ;
-		
-		int[] newArry =  new int[sucInt.length()];
-		
-		for (int i = 0; i < newArry.length; i++) {
-			newArry[i] = sucInt.charAt(i) - 48;
+		num++;
+
+		int[] newArray = new int[digitCount(num)];
+
+		for (int i = newArray.length-1; i >=0 ; i--) {
+			newArray[i] = num%10;
+			num = num/10;
 		}
-		
-		System.out.println(Arrays.toString(newArry));
+
+		System.out.println(Arrays.toString(newArray));
 	}
-	
-	
+
+	private static int digitCount(int x) {
+		int count = 0;
+
+		while (x>0) {
+			count++;
+			x = x/10;			
+		}
+		return count;	
+
+	}
 
 }
